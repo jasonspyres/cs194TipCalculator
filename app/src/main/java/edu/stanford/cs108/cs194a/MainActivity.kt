8ba1bb6d84489d695493eed70a49ffc59d6b4f2c
jasonspyres <jasonspyres@gmail.com>
2020-10-01 02:08:13 -0500

    I am learning how to committ stuff

---
+++ app/src/main/java/edu/stanford/cs108/cs194a/MainActivity.kt
@@ -0,0 +1,16 @@
+package edu.stanford.cs108.cs194a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //adding a comment for git
+        val r = 50
+        val changeMe = 4
+
+    }
+}
